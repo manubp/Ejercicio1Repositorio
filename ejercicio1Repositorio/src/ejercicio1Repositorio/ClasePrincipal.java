fe1c200a14443cfa4c66923b1c03ed2a18614fe0
manubp <manuelbarroso@outlook.es>
2019-03-29 10:02:37 +0100

    Creamos la clase principal

---
+++ ejercicio1Repositorio/src/ejercicio1Repositorio/ClasePrincipal.java
@@ -0,0 +1,16 @@
+package ejercicio1Repositorio;
+
+public class ClasePrincipal {
+	public ClasePrincipal(Vista vista, Modelo modelo) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) { 
+		
+		Vista vista = new Vista();
+		Modelo modelo = new Modelo();
+		new ClasePrincipal(vista, modelo);
+	
+		
+	}
+}
