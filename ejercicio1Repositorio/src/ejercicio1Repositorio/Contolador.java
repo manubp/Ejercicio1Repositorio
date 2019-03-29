d3e53fb4e96e303cfb2e8d54be6f2e516254d96d
manubp <manuelbarroso@outlook.es>
2019-03-29 09:57:21 +0100

    Creamos el controlador

---
+++ ejercicio1Repositorio/src/ejercicio1Repositorio/Contolador.java
@@ -0,0 +1,59 @@
+package ejercicio1Repositorio;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class Contolador implements ActionListener, WindowListener {
+
+	
+	
+	
+	
+	
+	
+	
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		System.exit(0); 
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+	}
+
+}
